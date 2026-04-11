8bcabafd5a278be4042cbca883a0720ec1e8b6f3
Tom Boone <tbc@tomboone.com>
2026-04-11 16:07:11 -0400

    update cors

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "cors_origins" {
   description = "List of allowed CORS origins"
   type        = list(string)
-  default     = ["https://boone.gift", "https://boone.christmas"]
+  default     = ["https://app.boone.gift", "https://app.boone.christmas"]
 }
