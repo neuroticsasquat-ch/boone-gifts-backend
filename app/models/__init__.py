ba41789a416789f0b83f921861a8b0684f8d8fe8
Tom Boone <tbc@tomboone.com>
2026-06-23 16:38:05 -0400

    feat(families): add family_invites model and M3 migration (NEU-342) (#96)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -9,6 +9,7 @@ from app.models.collection_item import CollectionItem
 from app.models.password_reset_token import PasswordResetToken
 from app.models.family import Family
 from app.models.family_member import FamilyMember
+from app.models.family_invite import FamilyInvite
 
 __all__ = [
     "User",
@@ -22,4 +23,5 @@ __all__ = [
     "PasswordResetToken",
     "Family",
     "FamilyMember",
+    "FamilyInvite",
 ]
